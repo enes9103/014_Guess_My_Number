654bee7fdfcd1e4befefbe56b9bc7d166c909ae2
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2022-02-16 23:13:10 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+//Selectors
+let againButton = document.querySelector(".again__button");
+let number = document.getElementById("number");
+let checkButton = document.querySelector(".check__button");
+let exactNumber = document.getElementById("exact__number2");
+let soru = document.querySelector(".soru");
+let trueFalse = document.querySelector(".true-false");
+let highscore = document.getElementById("hightscore");
+let guessTime = document.querySelector(".guess_time");
+let cong = document.querySelector(".cong");
+
+
+//Events
+checkButton.addEventListener("click", check1);
+againButton.addEventListener("click", again);
+
+
+//Functions
+function check1() {
+    if (number.value == exactNumber.value) {
+        trueFalse.innerHTML = "Congratulations You know it."
+        trueFalse.style.color = "green"
+        soru.style.visibility = "hidden";
+        guessTime.innerHTML = 0
+        highscore.innerHTML++
+    } else if (Number(number.value) > Number(exactNumber.value)) {
+        trueFalse.innerHTML = "Please go down"
+        trueFalse.style.color = "red"
+        guessTime.innerHTML++
+        number.value = ""
+    } else if (Number(number.value) < Number(exactNumber.value)) {
+        trueFalse.innerHTML = "Please go higher"
+        trueFalse.style.color = "red"
+        guessTime.innerHTML++
+        number.value = ""
+    }
+
+    if (Number(guessTime.innerHTML) >= 10) {
+        highscore.innerHTML--;
+        soru.style.visibility = "visible";
+        trueFalse.innerHTML = "lets start again";
+        guessTime.innerHTML = 0
+        exactNumber.value = Math.floor(Math.random() * 101)
+    }
+
+    if (highscore.innerHTML >= 20) {
+        cong.innerHTML = `Congratulations ${username}.You win the game`
+    } else if (highscore.innerHTML < 0) {
+        highscore.style.color = "red"
+    } else if (highscore.innerHTML > 0) {
+        highscore.style.color = "green"
+    }
+}
+
+function again() {
+    number.value = ""
+    exactNumber.value = Math.floor(Math.random() * 101)
+    soru.style.visibility = "visible"
+    trueFalse.innerHTML = "Lets Start"
+}
